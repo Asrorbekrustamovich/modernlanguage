512b6046d10b9c2bdbf5bbc5b7a482aacc18135f
Asrorbekrustamovich <asrorrustamovich007@gmail.com>
2026-03-24 09:40:49 +0500

    pdf generator added

---
+++ generate_pdf.py
@@ -0,0 +1,9 @@
+from fpdf import FPDF
+
+pdf = FPDF()
+pdf.add_page()
+pdf.set_font("Arial", size=12)
+pdf.cell(200, 10, txt="Bu sinov uchun yaratilgan PDF hujjat.", ln=1, align='C')
+pdf.cell(200, 10, txt="Eski qo'lyozma matni namunasi: 'Assalomu alaykum'.", ln=2, align='C')
+pdf.output("test_document.pdf")
+print("PDF created successfully.")
